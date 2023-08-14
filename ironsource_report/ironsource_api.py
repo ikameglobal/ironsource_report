a31f73b08d4a9b70971cb529891bb89b723cc380
Mung Vu <mungvt@Mungs-Mac-mini.local>
2023-08-14 18:02:29 +0700

    feat: get appkey, user ad revenue report

--- ironsource_report/ironsource_api.py
+++ ironsource_report/ironsource_api.py
@@ -1,30 +1,30 @@
-import logging
-import pandas as pd
 import requests
-from pandas import DataFrame
 
-from applovin_report.utils.datetime_utils import day_ago
-from applovin_report.utils.logging_utils import logging_basic_config
+from ironsource_report.utils.logging_utils import logging_basic_config
 from requests.adapters import HTTPAdapter, Retry
 
 logging_basic_config()
 STATUS_RETRIES = (500, 502, 503, 504)
 
 
-class AdRevenueMeasurements:
+class IronSourceClient:
     """
     Detailed documentation for this API can be found at:
         [ironSource Impression Level API](
         https://developers.is.com/ironsource-mobile/air/ad-revenue-measurements/#step-1
         )
     """
+    BASE_URL = 'https://platform.ironsrc.com'
+    API_AUTH = f'{BASE_URL}/partners/publisher/auth'
+    API_APPLICATION = f"{BASE_URL}/partners/publisher/applications/v6?"
+    API_AD_REVENUE = f"{BASE_URL}/partners/adRevenueMeasurements/v3"
 
-    def __init__(self, api_key: str | list[str],
+    def __init__(self, api_credential: dict,
                  status_retries: list[int] = STATUS_RETRIES,
                  max_retries=5, retry_delay=1):
         """
         Args:
-            api_key: API key(s) to use for the report
+            api_credential: API key(s) to use for the report
             status_retries: A set of HTTP status codes that we should force a retry on
             max_retries: Total number of retries to allow
             retry_delay: Num of seconds sleep between attempts
@@ -35,7 +35,23 @@ class AdRevenueMeasurements:
         Doc Author:
             mungvt@ikameglobal.com
         """
-        self.api_key = api_key
+        self._api_credential = api_credential
+        self._api_key = self.__get_bearer_token()
+        self._api_headers = {'Authorization': f'Bearer {self._api_key}'}
+
         self.session = requests.Session()
         retries = Retry(total=max_retries, backoff_factor=retry_delay, status_forcelist=status_retries)
         self.session.mount('https://', HTTPAdapter(max_retries=retries))
+
+    def __get_bearer_token(self):
+        """
+            Get bearer token for ironSource apps. Requires "secretkey" and "refreshToken". Returns a Bearer token.
+        """
+        auth_headers = {
+            "secretkey": self._api_credential['secretkey'],
+            "refreshToken": self._api_credential['refreshToken']
+        }
+        # Call the auth API to get Bearer token
+        response = requests.get(self.API_AUTH, headers=auth_headers)
+        bearer_token = response.text.replace('"', '')
+        return bearer_token
--- ironsource_report/ironsource_user_ad_revenue_reporting_api.py
+++ ironsource_report/ironsource_user_ad_revenue_reporting_api.py
@@ -1,17 +1,16 @@
 import logging
 import pandas as pd
-import requests
 from pandas import DataFrame
 
 from ironsource_report.utils.datetime_utils import day_ago
 from ironsource_report.utils.logging_utils import logging_basic_config
-from requests.adapters import HTTPAdapter, Retry
+from ironsource_api import IronSourceClient
 
 logging_basic_config()
 STATUS_RETRIES = (500, 502, 503, 504)
 
 
-class AdRevenueMeasurements:
+class AdRevenueMeasurements(IronSourceClient):
     """
     Detailed documentation for this API can be found at:
         [ironSource Impression Level API](
@@ -19,14 +18,12 @@ class AdRevenueMeasurements:
         )
     """
 
-    ENDPOINT = "https://platform.ironsrc.com/partners/adRevenueMeasurements/v3"
-
-    def __init__(self, api_key: str | list[str],
+    def __init__(self, api_credential: dict,
                  status_retries: list[int] = STATUS_RETRIES,
                  max_retries=5, retry_delay=1):
         """
         Args:
-            api_key: API key(s) to use for the report
+            api_credential: API key(s) to use for the report
             status_retries: A set of HTTP status codes that we should force a retry on
             max_retries: Total number of retries to allow
             retry_delay: Num of seconds sleep between attempts
@@ -37,16 +34,14 @@ class AdRevenueMeasurements:
         Doc Author:
             mungvt@ikameglobal.com
         """
-        self.api_key = api_key
-        self.session = requests.Session()
-        retries = Retry(total=max_retries, backoff_factor=retry_delay, status_forcelist=status_retries)
-        self.session.mount('https://', HTTPAdapter(max_retries=retries))
+        super().__init__(api_credential=api_credential, status_retries=status_retries, max_retries=max_retries,
+                         retry_delay=retry_delay)
 
     def get_report(
-            self,
-            date: str = day_ago(1),
-            app_key: str = "",
-            **kwargs
+        self,
+        date: str = day_ago(1),
+        app_key: str = "",
+        **kwargs
     ) -> DataFrame:
         """
         Retrieve a report from the ironSource Impression Level Revenue Server-Side API.
@@ -69,32 +64,32 @@ class AdRevenueMeasurements:
             **kwargs,
         }
 
-        headers = {
-            'Authorization': f'Bearer {self.api_key}'
-        }
-
-        response = self.session.get(url=self.ENDPOINT, params=params, headers=headers)
-        if response.status_code == 404:
-            logging.warning(response.text + '. Skipped it.')
-            return pd.DataFrame()
-        else:
+        response = self.session.get(url=self.API_AD_REVENUE, params=params, headers=self._api_headers)
+        if response.status_code == 200:
             report_file_urls = response.json()['urls']
             logging.info('Found {} report file(s)'.format(len(report_file_urls)))
             report_dfs = list(map(self._handle_report_file, report_file_urls))
             # Concat DFs
             result = pd.concat(report_dfs).reset_index().drop(columns=['index'])
             return result
+        else:
+            logging.warning(response.text + '. Skipped it.')
+            return pd.DataFrame()
 
     @staticmethod
-    def _handle_report_file(url):
+    def _handle_report_file(url: str):
         result = pd.read_csv(url, compression='gzip', dtype={
             'advertising_id': str,
             'ad_network': str,
             'revenue': str
         })  # Read report to DF
+        try:
+            short_url = url.split('.csv')[0]
+        except IndexError:
+            short_url = url
 
         if result.empty:
-            logging.warning(f"Not found data in report file at url: {url}.")
+            logging.warning(f"Not found data in report file at url: {short_url}.")
         else:
             logging.info(f"Collected successful ad revenue report file at url: {url}.")
         return result
